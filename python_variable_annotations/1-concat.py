363cf3a2c98707715279b1dacfb359b2a4b87c8a
Alfaleh1992 <imis.190@gmail.com>
2025-07-29 16:29:18 +0000

    a

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Function returns two concatenated strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Parameters
+        str1: str
+        str2: str
+
+    Return
+        str1 + str2
+    """
+
+    return str1 + str2
