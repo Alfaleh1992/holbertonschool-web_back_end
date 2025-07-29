2077c43c94daa2fc32a9bb673eb605588e1a201b
Alfaleh1992 <imis.190@gmail.com>
2025-07-29 16:27:45 +0000

    a

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Function returns the sum of two floats a and b
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Parameters
+        a: float
+        b: float
+
+    Return
+        a + b
+    """
+
+    return a + b
